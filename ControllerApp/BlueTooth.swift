cfb99dcca657cbf06f2aab4b351e95efaa11b8fa
RedWhite-cmd <sreddythatiparthi22@gmail.com>
2025-03-14 19:11:26 -0500

    Finished the blutooth feature in Xcode

--- ControllerApp/BlueTooth.swift
+++ ControllerApp/BlueTooth.swift
@@ -174,3 +174,4 @@ class CharacteristicFunction {
         return characteristic
     }
 }
+
--- ControllerApp/ContentView.swift
+++ ControllerApp/ContentView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var goToBluetooth = false
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: BluetoothView()) {
-                    Button(action: {}) {
+                    Button(action: { goToBluetooth = true }) {
                         Image("Bluetooth")
                     }
-                }
+                    .fullScreenCover(isPresented: $goToBluetooth){
+                        BluetoothView()
+                    }
                 
                 Spacer()
                 
@@ -31,22 +33,55 @@ struct ContentView: View {
     }
 }
 
+
 struct BluetoothView: View {
+    @State private var goToHome = false
+    @ObservedObject var btInterface = BTInterface.bluetooth
     var body: some View {
         VStack {
-            Text("Bluetooth Screen")
-                .font(.largeTitle)
-                .padding()
+            
+            Button(action: { goToHome = true }) {
+                Image("User")
+            }
+            .fullScreenCover(isPresented: $goToHome){
+                ContentView()
+            }
             
             Spacer()
-        }
+            
+            Button(action: {btInterface.service.toggleScan()}){
+                            Text(btInterface.service.scState == .scanning ? "Stop Scan" : "Start Scan")
+                        }
+                        .padding()
+
+            List(btInterface.peripheralsData, id: \.identifier) { peripheral in
+            HStack {
+                                Text(peripheral.name ?? "Unknown")
+                                Spacer()
+                                Circle()
+                                    .fill(btInterface.service.connectionColor(peripheral: peripheral))
+                                    .frame(width: 10, height: 10)
+            Button(action: {btInterface.service.centralManager.connect(peripheral, options: nil)}) {Text("Connect")}
+                            }
+                        }
+                        
+            Button(action: {if let firstCharacteristicFunction = btInterface.characteristicFunctionList.first {
+                                firstCharacteristicFunction.sendData(string: "Button Clicked")
+                            }
+                        }) {
+                            Text("Send Data on Button Click")
+                        }
+                    }
+
         .navigationTitle("Bluetooth")
     }
 }
+
+
 #Preview{
     ContentView()
 }
+
 #Preview{
     BluetoothView()
 }
-
