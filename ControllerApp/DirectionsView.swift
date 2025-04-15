6f6bcc9ac9ec9da580a006b1f5ce35856f07a910
RedWhite-cmd <sreddythatiparthi22@gmail.com>
2025-04-14 21:11:46 -0500

    Added the Info feature - Latest

---
+++ ControllerApp/DirectionsView.swift
@@ -0,0 +1,8 @@
+//
+//  DirectionsView.swift
+//  ControllerApp
+//
+//  Created by Sudeep Thatiparthi on 4/11/25.
+//
+
+import Foundation
---
+++ ControllerApp/InfoOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoOverlayView.swift
+//  ControllerApp
+//
+//  Created by Sudeep Thatiparthi on 4/14/25.
+//
+
+import SwiftUI
+
+struct InfoOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoOverlayView()
+}
